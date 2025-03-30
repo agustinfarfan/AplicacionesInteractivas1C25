14a4aeeca7cd208dd2bec37663e1930c462a87fc
valennesci <valen52nesci@gmail.com>
2025-03-30 16:49:20 -0300

    Productos y Publicacion

--- Clase Hibernate/demo/src/main/java/com/uade/tpo/demo/entity/Publicacion.java
+++ Clase Hibernate/demo/src/main/java/com/uade/tpo/demo/entity/Publicacion.java
@@ -1,7 +1,25 @@
 package com.uade.tpo.demo.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.Date;
 
+@Data
 @Entity
 public class Publicacion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "producto_id", referencedColumnName = "id", nullable = false)
+    private Producto producto;
+
+    @ManyToOne
+    @JoinColumn(name = "vendedor_id", referencedColumnName = "id", nullable = false)
+    private Usuario vendedor;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date fechaPublicacion;
 }
