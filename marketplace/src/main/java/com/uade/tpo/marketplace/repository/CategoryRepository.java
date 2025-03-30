a513ab972aec0f2d056713a891b1d43285490110
valennesci <valen52nesci@gmail.com>
2025-03-30 15:18:07 -0300

    Hibernate

--- marketplace/src/main/java/com/uade/tpo/marketplace/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.uade.tpo.marketplace.repository;
-
-import com.uade.tpo.marketplace.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
