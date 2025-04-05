9648479b8f36e6bf1583b40faae1154b4ab24cc3
nico-navidad2021 <nicomartinez@uade.edu.ar>
2025-04-05 10:12:53 -0300

    Modificaciones de ClienteRepository

--- Clase Hibernate/demo/src/main/java/com/uade/tpo/demo/repository/ClienteRepository.java
+++ Clase Hibernate/demo/src/main/java/com/uade/tpo/demo/repository/ClienteRepository.java
@@ -2,6 +2,13 @@ package com.uade.tpo.demo.repository;
 
 import com.uade.tpo.demo.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    
+    @Query(value = "select c from Cliente c where c.cuil = ?1")
+    Cliente findByCuil(String cuil);
+
+    @Query(value = "select c from Cliente c where c.razonSocial = ?1")
+    Cliente findByRazonSocial(String razonSocial);
 }
