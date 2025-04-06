67c4de4353e87167226fbe03eb2cbf32fda57f08
nico-navidad2021 <nicomartinez@uade.edu.ar>
2025-04-06 02:44:12 -0300

    Adapto metodo para que tome en cuenta 'contiene' del string

--- Clase Hibernate/demo/src/main/java/com/uade/tpo/demo/repository/ClienteRepository.java
+++ Clase Hibernate/demo/src/main/java/com/uade/tpo/demo/repository/ClienteRepository.java
@@ -1,6 +1,9 @@
 package com.uade.tpo.demo.repository;
 
 import com.uade.tpo.demo.entity.Cliente;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -9,6 +12,5 @@ public interface ClienteRepository extends JpaRepository<Cliente, Long> {
     @Query(value = "select c from Cliente c where c.cuil = ?1")
     Cliente findByCuil(String cuil);
 
-    @Query(value = "select c from Cliente c where c.razonSocial = ?1")
-    Cliente findByRazonSocial(String razonSocial);
+    List<Cliente> findByRazonSocialContainingIgnoreCase(String razonSocial);
 }
