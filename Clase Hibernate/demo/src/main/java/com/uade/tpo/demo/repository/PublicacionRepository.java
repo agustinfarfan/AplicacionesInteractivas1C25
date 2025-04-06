e12ab1d0b21e343309d78799aee5ea522ca15693
nico-navidad2021 <nicomartinez@uade.edu.ar>
2025-04-06 10:54:16 -0300

    Agrego la notation de Repository porque pincha

--- Clase Hibernate/demo/src/main/java/com/uade/tpo/demo/repository/PublicacionRepository.java
+++ Clase Hibernate/demo/src/main/java/com/uade/tpo/demo/repository/PublicacionRepository.java
@@ -2,6 +2,8 @@ package com.uade.tpo.demo.repository;
 
 import com.uade.tpo.demo.entity.Publicacion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PublicacionRepository extends JpaRepository<Publicacion, Long> {
 }
