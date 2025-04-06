9767316b0a8b6b95991a8e28c8dbb65d302bf62a
nico-navidad2021 <nicomartinez@uade.edu.ar>
2025-04-06 02:44:50 -0300

    Sincro temporal para lo que llevo hecho del Controller

---
+++ Clase Hibernate/demo/src/main/java/com/uade/tpo/demo/controllers/ClientesController.java
@@ -0,0 +1,77 @@
+package com.uade.tpo.demo.controllers;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.uade.tpo.demo.entity.Categoria;
+import com.uade.tpo.demo.entity.Cliente;
+import com.uade.tpo.demo.entity.dto.CategoriaRequest;
+import com.uade.tpo.demo.exceptions.CategoriaNotFoundExcepcion;
+import com.uade.tpo.demo.exceptions.CategoriaDuplicadaExcepcion;
+import com.uade.tpo.demo.service.CategoriaService;
+import com.uade.tpo.demo.service.ClienteService;
+
+@RestController
+@RequestMapping("clientes")
+public class ClientesController {
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @GetMapping
+    public ResponseEntity<Page<Cliente>> getClientes(
+            @RequestParam(required = false) Integer page,
+            @RequestParam(required = false) Integer size) {
+        if (page == null || size == null)
+            return ResponseEntity.ok(clienteService.getClientes(PageRequest.of(0, Integer.MAX_VALUE)));
+        return ResponseEntity.ok(clienteService.getClientes(PageRequest.of(page, size)));
+    }
+
+    @GetMapping("/{clienteId}")
+    public ResponseEntity<Cliente> getClienteById(@PathVariable Long clienteId) {
+        Optional<Cliente> result = clienteService.getClienteById(clienteId);
+        if (result.isPresent())
+            return ResponseEntity.ok(result.get());
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Categoria>> searchCategoriaByNombre(@PathVariable String categoryName) 
+            throws CategoriaNotFoundExcepcion {
+        List<Categoria> result = categoriaService.searchCategoriaByNombre(categoryName);
+        if (!result.isEmpty())
+            return ResponseEntity.ok(result);
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> createCategoria(@RequestBody CategoriaRequest categoriaRequest)
+            throws CategoriaDuplicadaExcepcion {
+
+                System.out.println("Dentro de createCategoria CategoriasController nombre: " + categoriaRequest.getNombre());
+        Categoria result = categoriaService.createCategoria(categoriaRequest.getNombre());
+        return ResponseEntity.created(URI.create("/categories/" + result.getId())).body(result);
+    }
+    
+    @DeleteMapping("/{categoryId}")
+    public ResponseEntity<Object> delCategoriaById(@PathVariable Long categoryId)
+            throws CategoriaNotFoundExcepcion {
+                categoriaService.delCategoriaById(categoryId);
+                return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{categoryId}")
+    public ResponseEntity<Categoria> actualizarCategoria(@PathVariable Long categoryId, @RequestBody CategoriaRequest categoriaRequest)
+            throws CategoriaNotFoundExcepcion {
+        return ResponseEntity.ok(categoriaService.updateCategoria(categoryId,categoriaRequest.getNombre(),categoriaRequest.getDescripcion()));
+    }
+}
