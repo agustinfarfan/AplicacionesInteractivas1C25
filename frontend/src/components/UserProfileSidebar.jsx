1ac375666487c3d0176d70b8b347d88043b7e839
nico-navidad2021 <nicomartinez@uade.edu.ar>
2025-06-01 22:15:21 -0300

    Agrego menu de configuracion para el admin

--- frontend/src/components/UserProfileSidebar.jsx
+++ frontend/src/components/UserProfileSidebar.jsx
@@ -5,7 +5,8 @@ import {
   HiOutlineClipboardList,
   HiOutlineUser,
   HiOutlineLocationMarker,
-  HiOutlineCheckCircle
+  HiOutlineCheckCircle,
+  HiOutlineCog
 } from "react-icons/hi";
 
 const UserProfileSidebar = ({ onClose }) => {
@@ -51,6 +52,21 @@ const UserProfileSidebar = ({ onClose }) => {
         
         <button className="text-left px-4 py-2 text-red-600 hover:text-red-800">Cerrar sesión</button>
       </div>
+        <details className="group mt-6">
+            <summary className="flex items-center justify-between cursor-pointer px-4 py-2 text-sm text-gray-700 hover:text-indigo-600 rounded-lg hover:bg-indigo-50">
+            <span className="flex items-center gap-2">
+                <HiOutlineCog className="text-lg" />
+                Configuracion
+            </span>
+            
+            </summary>
+            <div className="mt-2 ml-6 flex flex-col gap-1 text-sm">
+                <Link to="/admin/categories">Categorías</Link>
+                <Link to="/admin/productss">Productos</Link>
+                <Link to="/admin/clients">Clientes</Link>
+            </div>
+        </details>
+
     </div>
   );
 };
