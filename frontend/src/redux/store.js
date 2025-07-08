5286a995b8deea22792154ec732a41d3961488d1
valennesci <valen52nesci@gmail.com>
2025-07-08 19:33:07 -0300

    Cupones

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import carritoReducer from './carrito/carritoReducer'
 import authReducer from './user/authReducer' 
 import productosReducer from './productos/productosReducer' 
+import cuponesReducer from './cupones/cuponesReducer' 
 
 export const store = configureStore({
   reducer: {
     carrito: carritoReducer,
     user: authReducer,
-    productos: productosReducer
+    productos: productosReducer,
+    cupones: cuponesReducer
   },
 })
