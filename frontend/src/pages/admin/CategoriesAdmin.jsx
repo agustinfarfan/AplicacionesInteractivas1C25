2e01bd41247022d69114143c168c2008128d2ec5
nico-navidad2021 <nicomartinez@uade.edu.ar>
2025-07-09 16:50:12 -0300

    Me quedo colgado un comentario del delete

--- frontend/src/pages/admin/CategoriesAdmin.jsx
+++ frontend/src/pages/admin/CategoriesAdmin.jsx
@@ -310,7 +310,6 @@ const CategoriasAdmin = () => {
                   Cancelar
                 </button>
                 <button
-                  /*onClick={confirmDelete}*/
                   onClick={handleDelete}
                   className="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700"
                 >
