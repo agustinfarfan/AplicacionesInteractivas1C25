f5dd24b0e513fb1411e3e9480801c4e7a30e4a86
nico-navidad2021 <nicomartinez@uade.edu.ar>
2025-07-09 19:05:10 -0300

    Merge branch 'dev' of https://github.com/agustinfarfan/AplicacionesInteractivas1C25 into dev

--- frontend/src/redux/api/categoryProductApi.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_URL = 'http://localhost:4002'; 
-
-export const fetchProductsByCategory = async (categoryId) => {
-  const res = await axios.get(`${API_URL}/productos/categoria/${categoryId}`);
-  return res.data;
-};
